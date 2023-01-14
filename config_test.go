3ca9436e6581f111b009a356dee9dc40b879d3d8
John Whitton <john@johnwhitton.com>
2023-01-13 19:09:41 -0800

    Enhance config_test to check multiple owners for domains

--- config_test.go
+++ config_test.go
@@ -31,6 +31,7 @@ func TestConfig(t *testing.T) {
 	// Test we can connect to the client
 	// Test we can get configured contract addresses and they match
 	//Functional Tests
+
 	// Check Base Registrar is pointing to the ENS Registry
 	baseRegistrar, err := baseregistrar.NewContract(config.BaseRegistrar, config.client)
 	assert.Equal(t, err, nil, "Error getting BaseRegistrar")
@@ -40,20 +41,40 @@ func TestConfig(t *testing.T) {
 	// fmt.Printf("baseRegistrar: %v\n", &baseRegistrar)
 	assert.Equal(t, err, nil, "Error getting ENS from baseRegistrar")
 	assert.Equal(t, baseRegistrarENS, config.ENSRegistry, "Incorrect ENS for baseRegistrar")
-	//Check that the ENSRegistry has set the Deployer Address as the owner
-	deployerAddress := common.HexToAddress("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
-	nameHash, err := NameHash("resolver")
-	// assert.Equal(t, err, nil, "Error getting country Namehash")
-	// labelHash, err := LabelHash("resolver")
-	// assert.Equal(t, err, nil, "Error getting resolver Labelhash")
-	// sha := sha3.NewLegacyKeccak256()
-	// // nodeHash := sha.Sum(append(nameHash[:], labelHash[:]))
-	// nodeHash := sha3.NewLegacyKeccak256((append(nameHash[:], labelHash[:])))
-	// // resolverNode := "0x84a0d1d9a4103b566373dcaf0bd2d482e763a581452c91a0eafb77bb49a1a71d")
+
+	//Check that the ENSRegistry has test owners for domains set correctly
+	// await registerDomain('test', alice, await ensDeployer.publicResolver(), await ensDeployer.registrarController())
+	// await registerDomain('resolver', bob, await ensDeployer.publicResolver(), await ensDeployer.registrarController())
+	// deployerAddress := common.HexToAddress("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
+	aliceAddress := common.HexToAddress("0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65")
+	bobAddress := common.HexToAddress("0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc")
+	countryNameHash, err := NameHash("country")
+	assert.Equal(t, err, nil, "Error getting Namehash for country")
+	testNameHash, err := NameHash("test")
+	assert.Equal(t, err, nil, "Error getting Namehash for test")
+	resolverNameHash, err := NameHash("resolver")
+	assert.Equal(t, err, nil, "Error getting Namehash for resolver")
 	ensRegistry, err := ensregistry.NewContract(config.ENSRegistry, config.client)
 	assert.Equal(t, err, nil, "Error getting ENSRegistry")
-	resolverOwner, err := ensRegistry.Owner(nil, nameHash)
+	// country is owned by the BaseRegistrar
+	countryOwner, err := ensRegistry.Owner(nil, countryNameHash)
 	assert.Equal(t, err, nil, "Error getting resolver node owner form ENSRegistry")
-	assert.Equal(t, resolverOwner, deployerAddress, "Incorrect Owner for resolver node")
+	assert.Equal(t, countryOwner, config.BaseRegistrar, "Incorrect Owner for country node")
+	// test is owned by Alice
+	testOwner, err := ensRegistry.Owner(nil, testNameHash)
+	assert.Equal(t, err, nil, "Error getting resolver node owner form ENSRegistry")
+	assert.Equal(t, testOwner, aliceAddress, "Incorrect Owner for test node")
+	// resolver is owned by Bob
+	resolverOwner, err := ensRegistry.Owner(nil, resolverNameHash)
+	assert.Equal(t, err, nil, "Error getting resolver node owner form ENSRegistry")
+	assert.Equal(t, resolverOwner, bobAddress, "Incorrect Owner for resolver node")
 
 }
+
+// assert.Equal(t, err, nil, "Error getting country Namehash")
+// labelHash, err := LabelHash("resolver")
+// assert.Equal(t, err, nil, "Error getting resolver Labelhash")
+// sha := sha3.NewLegacyKeccak256()
+// // nodeHash := sha.Sum(append(nameHash[:], labelHash[:]))
+// nodeHash := sha3.NewLegacyKeccak256((append(nameHash[:], labelHash[:])))
+// // resolverNode := "0x84a0d1d9a4103b566373dcaf0bd2d482e763a581452c91a0eafb77bb49a1a71d")
